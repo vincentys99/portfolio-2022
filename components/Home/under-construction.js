567cd45d2639c639384f15b1cab670145743712a
vincentys99 <ysfung1704@gmail.com>
2022-08-02 17:47:17 +0800

    added AOS

--- components/Home/under-construction.js
+++ components/Home/under-construction.js
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import classes from "./under-construction.module.css";
 
 export default function UnderConstruction() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  }, []);
+
   return (
     <div className={classes.container}>
-      <h1>Under Construction</h1>
-      <p>come back later</p>
+      <span data-aos={"fade-down"}>🚧</span>
+      <h1 data-aos={"fade-right"}>Under Construction</h1>
+      <p data-aos={"fade-left"}>come back later</p>
     </div>
   );
 }
