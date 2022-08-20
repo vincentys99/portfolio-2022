36929badac2fd71bbe202fc423f862776d2269d8
vincentys99 <ysfung1704@gmail.com>
2022-08-20 22:01:07 +0800

    updates

--- components/simcompanies/index.js
+++ components/simcompanies/index.js
@@ -7,7 +7,7 @@ import BackToHome_Button from "../ui/btn_backToHome";
 
 import classes from "./index.module.css";
 
-function getFormOptions() {
+const getFormOptions = () => {
   const emptyList = [
     {
       id: "",
@@ -16,6 +16,7 @@ function getFormOptions() {
   ];
 
   const economyPhases = [
+    ...emptyList,
     {
       id: 0,
       value: "Recession",
@@ -31,6 +32,7 @@ function getFormOptions() {
   ];
 
   const buildings = [
+    ...emptyList,
     {
       id: "G",
       value: "Grocery Store",
@@ -55,14 +57,14 @@ function getFormOptions() {
       id: "A",
       value: "Gas Station",
     },
-    {
-      id: "B",
-      value: "Sales Office",
-    },
-    {
-      id: "r",
-      value: "Restaurant",
-    },
+    // {
+    //   id: "B",
+    //   value: "Sales Office",
+    // },
+    // {
+    //   id: "r",
+    //   value: "Restaurant",
+    // },
   ];
 
   const executives = ["COO", "CFO", "CMO", "CTO"];
@@ -70,49 +72,113 @@ function getFormOptions() {
   const results = { emptyList, economyPhases, buildings, executives };
 
   return results;
-}
+};
+
+const getValue = (name) => {
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    if (sessionStorage.getItem(name)) {
+      setValue(sessionStorage.getItem(name));
+    }
+  }, []);
+
+  return value;
+};
 
 export default function SimCompanies_Index() {
   const onSubmit_Handler = async function (event) {
     event.preventDefault();
 
-    const cooValues = [
+    const coo = [
       coo1_Ref.current.value ? coo1_Ref.current.value : 0,
       coo2_Ref.current.value ? coo2_Ref.current.value : 0,
       coo3_Ref.current.value ? coo3_Ref.current.value : 0,
       coo4_Ref.current.value ? coo4_Ref.current.value : 0,
     ];
 
-    const cfoValues = [
+    const cfo = [
       cfo1_Ref.current.value ? cfo1_Ref.current.value : 0,
       cfo2_Ref.current.value ? cfo2_Ref.current.value : 0,
       cfo3_Ref.current.value ? cfo3_Ref.current.value : 0,
       cfo4_Ref.current.value ? cfo4_Ref.current.value : 0,
     ];
 
-    const cmoValues = [
+    const cmo = [
       cmo1_Ref.current.value ? cmo1_Ref.current.value : 0,
       cmo2_Ref.current.value ? cmo2_Ref.current.value : 0,
       cmo3_Ref.current.value ? cmo3_Ref.current.value : 0,
       cmo4_Ref.current.value ? cmo4_Ref.current.value : 0,
     ];
 
-    const ctoValues = [
+    const cto = [
       cto1_Ref.current.value ? cto1_Ref.current.value : 0,
       cto2_Ref.current.value ? cto2_Ref.current.value : 0,
       cto3_Ref.current.value ? cto3_Ref.current.value : 0,
       cto4_Ref.current.value ? cto4_Ref.current.value : 0,
     ];
 
+    const economyPhase_Ref_value = economyPhase_Ref.current.value;
+    const buildingID_Ref_value = buildingID_Ref.current.value;
+    const buildingLvl_Ref_value = buildingLvl_Ref.current.value;
+    const resourceID_Ref_value = resourceID_Ref.current.value;
+    const cog_Ref_value = cog_Ref.current.value;
+    const quality_Ref_value = quality_Ref.current.value;
+    const runtime_Ref_value = runtime_Ref.current.value;
+    const coo1_Ref_value = coo1_Ref.current.value ?? 0;
+    const coo2_Ref_value = coo2_Ref.current.value ?? 0;
+    const coo3_Ref_value = coo3_Ref.current.value ?? 0;
+    const coo4_Ref_value = coo4_Ref.current.value ?? 0;
+    const cfo1_Ref_value = cfo1_Ref.current.value ?? 0;
+    const cfo2_Ref_value = cfo2_Ref.current.value ?? 0;
+    const cfo3_Ref_value = cfo3_Ref.current.value ?? 0;
+    const cfo4_Ref_value = cfo4_Ref.current.value ?? 0;
+    const cmo1_Ref_value = cmo1_Ref.current.value ?? 0;
+    const cmo2_Ref_value = cmo2_Ref.current.value ?? 0;
+    const cmo3_Ref_value = cmo3_Ref.current.value ?? 0;
+    const cmo4_Ref_value = cmo4_Ref.current.value ?? 0;
+    const cto1_Ref_value = cto1_Ref.current.value ?? 0;
+    const cto2_Ref_value = cto2_Ref.current.value ?? 0;
+    const cto3_Ref_value = cto3_Ref.current.value ?? 0;
+    const cto4_Ref_value = cto4_Ref.current.value ?? 0;
+    const totalBuildings_Ref_value = totalBuildings_Ref.current.value;
+    const recBonus_Ref_value = recBonus_Ref.current.value;
+
+    sessionStorage.setItem("economyPhase", economyPhase_Ref_value);
+    sessionStorage.setItem("buildingID", buildingID_Ref_value);
+    sessionStorage.setItem("buildingLvl", buildingLvl_Ref_value);
+    sessionStorage.setItem("resourceID", resourceID_Ref_value);
+    sessionStorage.setItem("cog", cog_Ref_value);
+    sessionStorage.setItem("quality", quality_Ref_value);
+    sessionStorage.setItem("runtime", runtime_Ref_value);
+    sessionStorage.setItem("coo1", coo1_Ref_value);
+    sessionStorage.setItem("coo2", coo2_Ref_value);
+    sessionStorage.setItem("coo3", coo3_Ref_value);
+    sessionStorage.setItem("coo4", coo4_Ref_value);
+    sessionStorage.setItem("cfo1", cfo1_Ref_value);
+    sessionStorage.setItem("cfo2", cfo2_Ref_value);
+    sessionStorage.setItem("cfo3", cfo3_Ref_value);
+    sessionStorage.setItem("cfo4", cfo4_Ref_value);
+    sessionStorage.setItem("cmo1", cmo1_Ref_value);
+    sessionStorage.setItem("cmo2", cmo2_Ref_value);
+    sessionStorage.setItem("cmo3", cmo3_Ref_value);
+    sessionStorage.setItem("cmo4", cmo4_Ref_value);
+    sessionStorage.setItem("cto1", cto1_Ref_value);
+    sessionStorage.setItem("cto2", cto2_Ref_value);
+    sessionStorage.setItem("cto3", cto3_Ref_value);
+    sessionStorage.setItem("cto4", cto4_Ref_value);
+    sessionStorage.setItem("totalBuildings", totalBuildings_Ref_value);
+    sessionStorage.setItem("recBonus", recBonus_Ref_value);
+
     const reqBody = {
       economyPhase: economyPhase_Ref.current.value,
       buildingID: buildingID_Ref.current.value,
       buildingLvl: buildingLvl_Ref.current.value,
-      productID: productID_Ref.current.value,
+      resourceID: resourceID_Ref.current.value,
       cog: cog_Ref.current.value,
       quality: quality_Ref.current.value,
       runtime: runtime_Ref.current.value,
-      executives: { cooValues, cfoValues, cmoValues, ctoValues },
+      executives: { coo, cfo, cmo, cto },
       totalBuildings: totalBuildings_Ref.current.value,
       recBonus: recBonus_Ref.current.value,
     };
@@ -126,8 +192,11 @@ export default function SimCompanies_Index() {
     });
     const data = await response.json();
     if (data) {
+      const { units, price, pphpl } = data;
       console.log(data);
-      setPopupText(JSON.stringify(data))
+      setPopupText(
+        `${units} units @ $${price.toLocaleString()} (PPHPL: ${pphpl.toLocaleString()})`
+      );
       setShowPopup(true);
     } else {
       console.log("Form submission error.");
@@ -161,7 +230,7 @@ export default function SimCompanies_Index() {
   const economyPhase_Ref = useRef();
   const buildingID_Ref = useRef();
   const buildingLvl_Ref = useRef();
-  const productID_Ref = useRef();
+  const resourceID_Ref = useRef();
   const cog_Ref = useRef();
   const quality_Ref = useRef();
   const runtime_Ref = useRef();
@@ -188,15 +257,16 @@ export default function SimCompanies_Index() {
     AOS.init({
       duration: 1500,
     });
-
-    getBuildingProducts(buildings[0].id);
   }, []);
 
   const [showPopup, setShowPopup] = useState(false);
   const [popupText, setPopupText] = useState("");
 
   const closePopup_Handler = (e) => {
-    console.log(e);
+    console.log(e.target.className);
+    if (e.target.className == `${classes.popupContainer}`) {
+      setShowPopup(false);
+    }
   };
 
   return (
@@ -267,11 +337,11 @@ export default function SimCompanies_Index() {
             </div>
           </div>
           <div className={classes.formGroup}>
-            <label htmlFor="productID">Product</label>
+            <label htmlFor="resourceID">Product</label>
             <select
-              id="productID"
+              id="resourceID"
               className={classes.formControl}
-              ref={productID_Ref}
+              ref={resourceID_Ref}
             >
               {buildingProducts.map((item) => {
                 return (
@@ -293,6 +363,7 @@ export default function SimCompanies_Index() {
                 className={classes.formControl}
                 ref={cog_Ref}
                 required
+                defaultValue={getValue("cog")}
               />
             </div>
             <div className={classes.formGroup}>
@@ -307,6 +378,7 @@ export default function SimCompanies_Index() {
                 className={classes.formControl}
                 ref={quality_Ref}
                 required
+                defaultValue={getValue("quality")}
               />
             </div>
           </div>
@@ -322,6 +394,7 @@ export default function SimCompanies_Index() {
               className={classes.formControl}
               ref={runtime_Ref}
               required
+              defaultValue={getValue("runtime")}
             />
           </div>
           {executives.map((item, index) => {
@@ -370,6 +443,7 @@ export default function SimCompanies_Index() {
                     pattern="\d*"
                     className={classes.formControl}
                     ref={ref1}
+                    defaultValue={getValue(`${item.toLowerCase()}1`)}
                   />
                   <input
                     id={`${item}2`}
@@ -379,6 +453,7 @@ export default function SimCompanies_Index() {
                     pattern="\d*"
                     className={classes.formControl}
                     ref={ref2}
+                    defaultValue={getValue(`${item.toLowerCase()}2`)}
                   />
                   <input
                     id={`${item}3`}
@@ -388,6 +463,7 @@ export default function SimCompanies_Index() {
                     pattern="\d*"
                     className={classes.formControl}
                     ref={ref3}
+                    defaultValue={getValue(`${item.toLowerCase()}3`)}
                   />
                   <input
                     id={`${item}4`}
@@ -397,6 +473,7 @@ export default function SimCompanies_Index() {
                     pattern="\d*"
                     className={classes.formControl}
                     ref={ref4}
+                    defaultValue={getValue(`${item.toLowerCase()}4`)}
                   />
                 </div>
               </div>
@@ -414,10 +491,11 @@ export default function SimCompanies_Index() {
                 placeholder="0 ~ 20"
                 className={classes.formControl}
                 ref={totalBuildings_Ref}
+                defaultValue={getValue("totalBuildings")}
               />
             </div>
             <div className={classes.formGroup}>
-              <label htmlFor="recBonus">Recreation Bonus (%)</label>
+              <label htmlFor="recBonus">Rec Bonus (%)</label>
               <input
                 id="recBonus"
                 type={"number"}
@@ -427,6 +505,7 @@ export default function SimCompanies_Index() {
                 placeholder="0 ~ 9"
                 className={classes.formControl}
                 ref={recBonus_Ref}
+                defaultValue={getValue("recBonus")}
               />
             </div>
           </div>
--- pages/api/simcompanies/result.js
+++ pages/api/simcompanies/result.js
@@ -3,24 +3,174 @@ const server = dev
   ? "http://localhost:3000"
   : "https://portfolio-2022-steel-chi.vercel.app/";
 
+async function getCompanyInfo() {
+  try {
+    const companyID = 801182;
+    const response = await fetch(
+      `https://www.simcompanies.com/api/v2/companies/${companyID}/`
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return;
+  }
+}
+
+async function getBuildingInfo(buildingID) {
+  try {
+    const response = await fetch(
+      `https://www.simcompanies.com/api/v3/0/encyclopedia/buildings/${buildingID}/`
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return;
+  }
+}
+
+async function getResourceInfo(economyPhase, resourceID) {
+  try {
+    const response = await fetch(
+      `https://www.simcompanies.com/api/v4/en/0/encyclopedia/resources/${economyPhase}/${resourceID}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return;
+  }
+}
+
+async function getTotalSalesModifier(salesModifier, executives, recBonus) {
+  const coo = parseInt(executives.coo[2]);
+  const cfo = parseInt(executives.cfo[2]);
+  const cmo = parseInt(executives.cmo[2]);
+  const cto = parseInt(executives.cto[2]);
+  const executivesBonus = parseInt(cmo / 3 + (coo + cfo + cto) / 12);
+
+  return (
+    parseInt(salesModifier) + parseInt(executivesBonus) + parseInt(recBonus)
+  );
+}
+
+async function getWagesPerHour(wages, buildingLvl, workers, executives) {
+  const coo = parseInt(executives.coo[0]);
+  const cfo = parseInt(executives.cfo[0]);
+  const cmo = parseInt(executives.cmo[0]);
+  const cto = parseInt(executives.cto[0]);
+  const executivesBonus = coo + parseInt((cfo + cmo + cto) / 4);
+  const adminRate = (workers - 100) / (170 * 100);
+  const netAdminRate = adminRate * (1 - executivesBonus / 100);
+
+  const wagesPerHour = wages * buildingLvl * (1 + netAdminRate);
+
+  return wagesPerHour;
+}
+
+async function calculateTimePerUnit(retailModeling, curPrice, saturation) {
+  const code = retailModeling
+    .replace("*amount", "")
+    .replace("price", curPrice.toString())
+    .replace("saturation", saturation.toString());
+
+  const timePerUnit = eval(code);
+  return timePerUnit;
+}
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      const economyPhase = req.body.economyPhase;
-      const buildingID = req.body.buildingID;
-      const productID = req.body.productID;
+      const {
+        economyPhase,
+        buildingID,
+        buildingLvl,
+        resourceID,
+        cog,
+        quality,
+        runtime,
+        executives,
+        totalBuildings,
+        recBonus,
+      } = req.body;
 
-      const response = await fetch(
-        `${server}/api/simcompanies/building/${buildingID}`
+      const companyInfo = await getCompanyInfo();
+      const buildingInfo = await getBuildingInfo(buildingID);
+      const resourceInfo = await getResourceInfo(economyPhase, resourceID);
+
+      const workers = companyInfo.company.workers;
+      const salesModifier = companyInfo.company.salesModifier;
+      const avgRetailPrice = resourceInfo.averageRetailPrice;
+      const marketSaturation = resourceInfo.marketSaturation;
+      const retailModeling = resourceInfo.retailModeling;
+
+      const totalSalesModifier = await getTotalSalesModifier(
+        salesModifier,
+        executives,
+        recBonus
       );
-      const data = await response.json();
 
-      res
-        .status(200)
-        .json({
-          buildingInfo: data,
-          ...req.body
-        });
+      const wagesPerHour = await getWagesPerHour(
+        buildingInfo.wages,
+        parseInt(buildingLvl),
+        workers,
+        executives
+      );
+
+      const avgRetailPrice_Rounded = Math.round(avgRetailPrice);
+
+      var res_unitsPerHour = [];
+      var res_unitsPerDay = [];
+      var res_profitsPerHour = [];
+      var res_profitsPerDay = [];
+      var res_sellPrice = [];
+
+      for (const x of Array(500).keys()) {
+        const curPrice = avgRetailPrice_Rounded + (x - 250);
+        const minSaturation = -0.38;
+        const finalSaturation = marketSaturation - 0.24 * quality;
+        const saturation =
+          finalSaturation > minSaturation ? finalSaturation : minSaturation;
+        const timePerUnit = await calculateTimePerUnit(
+          retailModeling,
+          curPrice,
+          saturation
+        );
+        var unitPerHour = 3600 / timePerUnit;
+
+        unitPerHour /= 1 - totalSalesModifier / 100;
+        unitPerHour *= parseInt(buildingLvl);
+
+        var profitPerUnit = curPrice - cog;
+        var profitPerHour = profitPerUnit * unitPerHour;
+
+        profitPerHour -= wagesPerHour;
+
+        var unitPerDay = unitPerHour * runtime;
+        var profitPerDay = profitPerHour * runtime;
+
+        res_unitsPerHour.push(unitPerHour);
+        res_unitsPerDay.push(unitPerDay);
+        res_profitsPerHour.push(profitPerHour);
+        res_profitsPerDay.push(profitPerDay);
+        res_sellPrice.push(curPrice);
+      }
+
+      const index = res_profitsPerHour.indexOf(Math.max(...res_profitsPerHour));
+      const selected_unitsPerDay = Math.round(res_unitsPerDay[index], 2);
+      const selected_sellPrice = res_sellPrice[index];
+
+      const pphpl = (res_profitsPerHour[index] / parseInt(buildingLvl)).toFixed(
+        2
+      );
+
+      res.status(200).json({
+        // companyInfo: companyInfo,
+        // buildingInfo: buildingInfo,
+        // resourceInfo: resourceInfo,
+        units: selected_unitsPerDay,
+        price: selected_sellPrice,
+        pphpl: pphpl,
+        // ...req.body,
+      });
     } catch (err) {
       res.status(400).json({ message: err });
     }
